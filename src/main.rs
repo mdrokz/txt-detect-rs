fe14104aeb220d7e9357c9f7922a9aee1b4f4d75
mdrokz <mohammadmunshi@gmail.com>
2022-12-30 19:01:55 +0530

    call train method with values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use east_model::train::train;
+
 fn main() {
-    println!("Hello, world!");
+
+    let train_img_path = "./ch4_training_images";
+	let train_gt_path  = "./ch4_training_localization_transcription_gt";
+	let pths_path      = "./pths";
+	let batch_size     = 24; 
+	let lr             = 1e-3;
+	let num_workers    = 4;
+	let epoch_iter     = 600;
+	let save_interval  = 5;
+
+
+    train(train_img_path.to_string(), train_gt_path.to_string(), pths_path.to_string(), batch_size, lr, num_workers, epoch_iter, save_interval);
+
 }
